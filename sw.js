4fe982475538d72de40cd03cd22f315d9535a85b
ParfeneAndrei <parfeneandrei22@yahoo.ro>
2026-07-15 13:48:17 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* APARM PWA service worker - offline app shell */
-var CACHE = 'aparm-v1';
+var CACHE = 'aparm-v2';
 var ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
 
@@ -17,6 +17,7 @@ self.addEventListener('activate', function (e) {
 /* strategie: retea intai pentru index (ca sa prinzi versiunile noi), cache ca rezerva offline */
 self.addEventListener('fetch', function (e) {
   if (e.request.method !== 'GET') return;
+  if (new URL(e.request.url).origin !== location.origin) return; // API-urile externe trec direct
   e.respondWith(
     fetch(e.request).then(function (resp) {
       var copy = resp.clone();
